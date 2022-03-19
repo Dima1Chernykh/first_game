c73c7d7f9300cc63c8bc92c4e2e2f13467b90ff8
Dmitriy <33864734+DimaChernykh@users.noreply.github.com>
2022-03-19 19:36:17 +0300

    complete v1

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -2,12 +2,21 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.TimeUtils;
+
+import java.awt.*;
+import java.util.Iterator;
 
 public class MyGdxGame extends ApplicationAdapter {
 	private Texture dropImage;
@@ -17,6 +26,14 @@ public class MyGdxGame extends ApplicationAdapter {
 
 	private OrthographicCamera camera;
 	private SpriteBatch batch;
+
+	private Rectangle bucket;
+
+	private Array<Rectangle> raindrops;
+	private long lastDropTime;
+
+	private int screenWidth = Toolkit.getDefaultToolkit().getScreenSize().width;
+	private int screenHeight = Toolkit.getDefaultToolkit().getScreenSize().height;
 
 	@Override
 	public void create() {
@@ -34,8 +51,80 @@ public class MyGdxGame extends ApplicationAdapter {
 
 		// add camera
 		camera = new OrthographicCamera();
-		camera.setToOrtho(false, 800, 480);
+		camera.setToOrtho(false, screenWidth, screenHeight);
 
+		// add batch for rendering
 		batch = new SpriteBatch();
+
+		// add bucket
+		bucket = new Rectangle();
+		bucket.x = screenWidth / 2 - 64 / 2;
+		bucket.y = 20;
+		bucket.width = 64;
+		bucket.height = 64;
+
+		// add raindrops
+		raindrops = new Array<>();
+		spawnRaindrop();
+
+	}
+
+	@Override
+	public void render() {
+		ScreenUtils.clear(0, 0, 0.2f, 1);
+
+		camera.update();
+
+		batch.setProjectionMatrix(camera.combined);
+		batch.begin();
+		batch.draw(bucketImage, bucket.x, bucket.y);
+		for(Rectangle raindrop: raindrops) {
+			batch.draw(dropImage, raindrop.x, raindrop.y);
+		}
+		batch.end();
+
+		if(Gdx.input.isTouched()) {
+			Vector3 touchPos = new Vector3();
+			touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+			camera.unproject(touchPos);
+			bucket.x = touchPos.x - 64 / 2;
+		}
+
+		if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) bucket.x -= 200 * Gdx.graphics.getDeltaTime();
+		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) bucket.x += 200 * Gdx.graphics.getDeltaTime();
+
+		if(bucket.x < 0) bucket.x = 0;
+		if(bucket.x > screenWidth - 64) bucket.x = screenWidth - 64;
+
+		if(TimeUtils.millis() - lastDropTime > MathUtils.random(0, 3000)) spawnRaindrop();
+
+		for (Iterator<Rectangle> iter = raindrops.iterator(); iter.hasNext(); ) {
+			Rectangle raindrop = iter.next();
+			raindrop.y -= 200 * Gdx.graphics.getDeltaTime();
+			if(raindrop.y + 64 < 0) iter.remove();
+			if(raindrop.overlaps(bucket)) {
+				dropSound.play();
+				iter.remove();
+			}
+		}
+	}
+
+	@Override
+	public void dispose() {
+		dropImage.dispose();
+		bucketImage.dispose();
+		dropSound.dispose();
+		rainMusic.dispose();
+		batch.dispose();
+	}
+
+	private void spawnRaindrop() {
+		Rectangle raindrop = new Rectangle();
+		raindrop.x = MathUtils.random(0, screenWidth - 64);
+		raindrop.y = screenHeight;
+		raindrop.width = 64;
+		raindrop.height = 64;
+		raindrops.add(raindrop);
+		lastDropTime = TimeUtils.millis();
 	}
 }
